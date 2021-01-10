d9ae9fdde4c1d3f6de5d9808cd01d6d4ea4b95fc
Suraj Das <44750499+thesurajdas@users.noreply.github.com>
2021-01-10 14:42:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var value = 
+    '<a href="https://soundfry.com/{:permalink}"><div class="sfb sfb-outline-primary"><i class="soundfry"></i> Follow</div></a><script async src="https://dl.dropboxusercontent.com/s/a64gkohkd5nmcmu/app.js" charset="utf-8"></script>';
+var input = document.getElementById('username');
+input.oninput = function (e) {
+  const textarea = document.getElementById('embed-url');
+  console.log(textarea.value);
+  textarea.value = value.replace('{:permalink}', encodeURIComponent(e.target.value));
+}
+var copy = document.getElementById('copy');
+copy.onclick = () => {
+   var _ = new ClipboardJS('#copy', {
+      text: function bob(trigger) {
+       return trigger.previousElementSibling.value;
+    }});
+}
